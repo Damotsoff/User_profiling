3b2dda1b17162e8e1cb10f0edee3a0c10b1014fe
George Damotsov <george.damotsov@gmail.com>
2022-06-05 18:14:37 +0300

    first commit

---
+++ script.sh
@@ -0,0 +1,7 @@
+echo "# User_profiling" >> README.md
+git init
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin git@github.com:Damotsoff/User_profiling.git
+git push -u origin main
